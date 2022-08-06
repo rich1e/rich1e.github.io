d167b00fb9bbba83023a87f5576662e1d55cf9ca
Richie <yuqi.gong.home@gmail.com>
2022-08-06 20:06:40 +0800

    add @vuepress/plugin-google-analytics by Version@1.x

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: gongyuqi
  * @Date: 2021-11-30 12:23:38
  * @LastEditors: rich1e
- * @LastEditTime: 2022-08-06 19:31:09
+ * @LastEditTime: 2022-08-06 19:56:21
  * @FilePath: /rich1e.me/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from "vuepress";
@@ -17,32 +17,32 @@ import markdownFootnote from "markdown-it-footnote";
 import VuepressPluginPermalinkPinyin from "./plugins/@permalink-pinyin";
 
 export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
-  head: [
-    /**
-     * vuepress配置谷歌统计
-     * @see https://www.sofineday.com/vuepress-google-analytics.html
-     */
-    /*************** start 添加谷歌统计 ***********/
-    [
-      "script",
-      {
-        src: "https://www.googletagmanager.com/gtag/js?id=G-LQXPNSFYVH",
-        async: true,
-      },
-    ],
-    [
-      "script",
-      {},
-      `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+  // head: [
+  //   /**
+  //    * vuepress配置谷歌统计
+  //    * @see https://www.sofineday.com/vuepress-google-analytics.html
+  //    */
+  //   /*************** start 添加谷歌统计 ***********/
+  //   [
+  //     "script",
+  //     {
+  //       src: "https://www.googletagmanager.com/gtag/js?id=G-LQXPNSFYVH",
+  //       async: true,
+  //     },
+  //   ],
+  //   [
+  //     "script",
+  //     {},
+  //     `
+  //       window.dataLayer = window.dataLayer || [];
+  //       function gtag(){dataLayer.push(arguments);}
+  //       gtag('js', new Date());
 
-        gtag('config', 'G-LQXPNSFYVH');
-      `,
-    ],
-    /*************** end 添加谷歌统计 ***********/
-  ],
+  //       gtag('config', 'G-LQXPNSFYVH');
+  //     `,
+  //   ],
+  //   /*************** end 添加谷歌统计 ***********/
+  // ],
 
   lang: "en-US",
   title: "Personal Hobbies",
@@ -73,6 +73,12 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
         tsLoaderOptions: {
           // ts-loader 的所有配置项
         },
+      },
+    ],
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-236278732-1", // UA-00000000-0
       },
     ],
     VuepressPluginPermalinkPinyin,
